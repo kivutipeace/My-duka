82ad75ec16cd8861e4c6599db69f3c446bdfda3f
Peace <kivutipeace@gmail.com>
2025-12-02 15:22:09 +0300

    OPP

--- OOP.py
+++ OOP.py
@@ -1,11 +1,12 @@
 class BankAccount:
+    # Constructor - initializes objects
     def __init__(self,account_number,balance,owner_name,date_opened):
         self.account_number = account_number
         self.balance = balance
         self.owner_name = owner_name
-        self.data_opened = date_opened
+        self.date_opened = date_opened
         
-        
+        # self - refrences the object
     def withdraw(self,amount):
         if amount <= self.balance:
             self.balance -= amount
@@ -13,18 +14,20 @@ class BankAccount:
             return "Insufficient funds"
         
     def deposit(self,amount):
-        self.amount+= amount
+        self.balance+= amount
         
     def display_info(self):
         return f"====Welcome {self.owner_name} account number {self.account_number}, balance is {self.balance}, account created at {self.date_opened}==="
-    def Account(self):
-        self.deposit(1000)
-        self.withdraw(500)
-        return self.display_info()
-        
     
-Account1 = BankAccount(1234,100000.00,"Peace",12-10-2004)
-Account2 = BankAccount(2345,200000.00,"Letting",13-10-2003)
+        
+# Object creation and method calling
+Account1 = BankAccount(1234,100000.00,"Peace","12-10-2004")
+Account2 = BankAccount(2345,200000.00,"Letting","13-10-2003")
+Account1.deposit(1000)
+Account1.withdraw(500)
+Account2.deposit(1000)
+Account2.withdraw(500)
+    
 print(Account1.display_info())
 print(Account2.display_info())
 
