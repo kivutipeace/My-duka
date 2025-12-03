cba865ca9e413f40c05eb0aa8c10fddf59bda061
Peace <kivutipeace@gmail.com>
2025-12-03 16:16:00 +0300

    initial commit

--- OOP.py
+++ OOP.py
@@ -44,7 +44,6 @@ class Car: # Class
         self.fuel_level = fuel_level
         self.is_running = is_running
         
-        # Behaviour
         # self - references the object
     def start(self):
         if self.fuel_level >= 100 and self.is_running == True:
@@ -52,48 +51,36 @@ class Car: # Class
         else:
             return 'Your car cant start' 
     
-    # Behaviour to stop the car if running
     def stop(self):
-        # Check if the car is running in order to stop the car
-        if self.is_running == True:
-            return 'The car is running press the stop button'
-        else: # else the car is already stopped
-            return 'The car is already on stop'
-            
-            
-    # Behaviour to refuel the car
+        if self.is_running == False:
+            return 'The is stopped'
+        else: 
+            return 'The car not stopped'
+
     def refuel(self, fuel_capacity, fuel_level, refuel_amount):
-    # Can't refuel if already full
-     if fuel_level >= fuel_capacity:
-        return "The car is already full"
+        if fuel_level >= fuel_capacity:
+          return "The car is already full"
     
-    # add the refueled amout 
-     new_level = fuel_level + refuel_amount
+        new_level = fuel_level + refuel_amount
 
-    # Cap it at fuel_capacity
-     if new_level > fuel_capacity:
-        new_level = fuel_capacity
+        if new_level > fuel_capacity:
+            new_level = fuel_capacity
 
-    # Update fuel level and return the new fuel level
-     self.fuel_level = new_level
-     return f"Refueled. New fuel level is : {self.fuel_level}"
+        self.fuel_level = new_level
+        return f"Refueled. New fuel level is : {self.fuel_level}"
 
-    # Behaviour to drive the car
     def drive(self,fuel_level):
-        # Checking if the car is running and the fuel level is enough
         if self.is_running == True and self.fuel_level == 10 <= fuel_level <=100 :
             print('You are driving the car')
-        else: # else the car is not in drive
+        else: 
             print('You are not driving the car')
                
-        # display the car info
     def display_info(self):
             return f"=== Your car {self.brand}, {self.model}, year {self.year}, fuel capacity {self.fuel_capacity}, current fuel level {self.fuel_level} is runnning {self.is_running} ==="   
             
 # Creating the objects and calling the method
 car1 = Car ('Subaru','Forester',2016,100,80,True)
 car1.refuel(100,70,80)
-# Printing the cars info
 print(car1.display_info())
 
 
@@ -113,7 +100,8 @@ class Student(Person): # Child class
         self.course = course
         
     def display_info(self):
-        return f"=== Student id: {self.student_id} and student course: {self.course} ==="
+        super().display_info()
+        print(f"=== Student id: {self.student_id} and student course: {self.course} ===")
 class Teacher(Person): # Child class
     def __init__(self,name,age,subject,salary):
         super().__init__(name,age)
@@ -121,24 +109,12 @@ class Teacher(Person): # Child class
         self.salary = salary
         
     def display_info(self):
-        return f"=== Teachers subject {self.subject} and teachers salary {self.salary} ==="
+        super.display_info()
+        print(f"=== Teachers subject {self.subject} \n and teachers salary {self.salary} ===")
     
 # Object construction 
 student = Student("Peace",15,90,"Computer Science")
 teacher = Teacher("Letting",25,"Computer Science",200000)
 # Printing the persons info
-print(student.display_info())
-print(teacher.display_info())
-
-
-
-
-userInput = input("Enter your name: ")
-if userInput == "Ken":
-    print('Not allowed')
-elif userInput == "Faridah":
-    print('Note allowed')
-elif userInput == "Peace":
-    print(Account1.display_info())
-else:
-    print('Not allowed')
+student.display_info()
+teacher.dispaly_info()
